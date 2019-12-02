c2e596e0674fc59f88fef7fbd526fe37bba50284
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2019-12-02 17:39:35 -0500

    loop platform select menu until user makes choice & then scrape games from choice

--- lib/cli.rb
+++ lib/cli.rb
@@ -1,20 +1,41 @@
 class CLI
   def run
     user_input = nil
+    type = nil
     puts "Games Coming Soon"
 
-    until user_input == 'quit'
-      puts <<-DOC
+    until user_input == 'quit' || user_input.to_i.between?(1, 5)
+      self.platform_select_menu
+      user_input = gets.strip
+       exit if user_input == 'quit'
+
+      case user_input.to_i
+      when 1
+        type = :all
+      when 2
+        type = :pc
+      when 3
+        type = :xb1
+      when 4
+        type = :ps4
+      when 5
+        type = :switch
+      else
+        puts "You entered an invalid choice."
+      end
+    end
+
+    Scraper.scrape_coming_soon_page(type)
+  end
+
+  def platform_select_menu
+    puts <<-DOC
       1. All
       2. PC
       3. Xbox One
       4. PlayStation 4
       5. Nintendo Switch
-
-      Enter the number corresponding to the platform you'd like to see upcoming games for, or type 'quit' to quit:
      DOC
-
-     user_input = gets.strip
-    end
+     print "Enter the number corresponding to the platform you'd like to see upcoming games for, or type 'quit' to quit: "
   end
 end
