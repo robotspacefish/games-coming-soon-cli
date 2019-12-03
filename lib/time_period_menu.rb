afe0443cda6fa0e35498feab4509da4aa9656b45
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2019-12-03 13:34:22 -0500

    delete blank space

--- lib/time_period_menu.rb
+++ lib/time_period_menu.rb
@@ -22,8 +22,6 @@ class TimePeriodMenu < Menu
         puts "\nYou selected #{@content[time_period]}.\n"
 
         Game.print_time_period_results(time_period, @content[time_period])
-
-
       elsif index == quit
         puts "\nThanks for using Games Coming Soon. Goodbye!\n"
         exit
