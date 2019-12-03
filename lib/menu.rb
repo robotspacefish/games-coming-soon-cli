3f3484fb0e7631019c2ca0e663bf31cb1c892fd1
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2019-12-03 17:29:33 -0500

    delete run from Menu module

--- lib/menu.rb
+++ lib/menu.rb
@@ -46,33 +46,4 @@ module Menu
 
     print "\n#{@instructions}"
   end
-
-  def run
-    quit = @content.length
-
-    until @input.to_i.between?(1, quit)
-      self.print_from_hash
-
-      @input = gets.strip
-      index = @input.to_i - 1
-
-      if index.between?(0, quit - 1)
-        selection = @content.to_a[index][0]
-        puts "\nYou selected #{content[selection]}.\n"
-
-        if self.class == MainMenu
-          puts "\nOne moment please...\n"
-          Scraper.scrape_coming_soon_page(selection)
-        elsif self.class == TimePeriodMenu
-          Game.print_time_period_results(selection,  @content[selection])
-        end
-
-      elsif index == quit
-        puts "\nThanks for using Games Coming Soon. Goodbye!\n"
-        exit
-      else
-        puts "\nYou made an invalid selection.\n"
-      end
-    end
-  end
 end
