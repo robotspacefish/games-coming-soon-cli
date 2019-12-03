51b495ca390591edad1eec42cd11198df6972d1e
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2019-12-03 14:16:25 -0500

    combine run methods for subclasses and move to parent class

--- lib/main_menu.rb
+++ lib/main_menu.rb
@@ -10,28 +10,4 @@ class MainMenu < Menu
 
       @instructions = "Enter the number corresponding to the platform you'd like to see upcoming games for, or type #{@content.length + 1} to quit: "
   end
-
-  def run
-    quit = @content.length
-
-    until @input.to_i.between?(1, quit)
-      self.print_from_hash
-
-      @input = gets.strip
-      index = @input.to_i - 1
-
-      if index.between?(0, quit - 1)
-        platform_type = @content.to_a[index][0]
-
-        puts "\nYou selected #{content[platform_type]}.\n"
-
-        Scraper.scrape_coming_soon_page(platform_type)
-      elsif index == quit
-        puts "\nThanks for using Games Coming Soon. Goodbye!\n"
-        exit
-      else
-        puts "\nYou made an invalid selection.\n"
-      end
-    end
-  end
 end
