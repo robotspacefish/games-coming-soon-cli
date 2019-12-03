83c37daad438cec540af7218ca93dfa8818fb994
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2019-12-02 22:36:30 -0500

    move main menu loop to MainMenu class

--- lib/cli.rb
+++ lib/cli.rb
@@ -4,38 +4,9 @@ class CLI
     type = nil
     puts "Games Coming Soon"
 
-    until user_input == 'quit' || user_input.to_i.between?(1, 5)
-      self.platform_select_menu
-      user_input = gets.strip
-       exit if user_input == 'quit'
+    MainMenu.new.run
 
-      case user_input.to_i
-      when 1
-        platform_type = :all
-      when 2
-        platform_type = :pc
-      when 3
-        platform_type = :xb1
-      when 4
-        platform_type = :ps4
-      when 5
-        platform_type = :switch
-      else
-        puts "You entered an invalid choice."
-      end
-    end
-
-    Scraper.scrape_coming_soon_page(platform_type)
-  end
-
-  def platform_select_menu
-    puts <<-DOC
-      1. All
-      2. PC (Windows)
-      3. Xbox One
-      4. PlayStation 4
-      5. Nintendo Switch
-     DOC
-     print "Enter the number corresponding to the platform you'd like to see upcoming games for, or type 'quit' to quit: "
+    binding.pry
+    # Scraper.scrape_coming_soon_page(platform_type)
   end
 end
--- lib/main_menu.rb
+++ lib/main_menu.rb
@@ -11,4 +11,23 @@ class MainMenu < Menu
 
       @instructions = "Enter the number corresponding to the platform you'd like to see upcoming games for, or type #{@content.length + 1} to quit: "
   end
+
+  def run
+    quit = @content.length
+    until @input.to_i.between?(1, quit)
+      self.print_from_hash
+
+      @input = gets.strip
+      index = @input.to_i - 1
+
+      if index >= 1 && index < quit
+        platform_type = @content.to_a[index][0]
+        Scraper.scrape_coming_soon_page(platform_type)
+      elsif index == quit
+        exit
+      else
+        puts "You made an invalid selection."
+      end
+    end
+  end
 end
