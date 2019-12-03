04bf3d3035a3088d506be76573e529f3f831a656
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2019-12-03 15:59:30 -0500

    delete files no longer needed

--- lib/time_period_menu.rb
+++
@@ -1,9 +0,0 @@
-class TimePeriodMenu < Menu
-  def initialize
-    @content = {
-      seven_days: "7 Days",
-      fourteen_days: "14 Days"
-    }
-    @instructions = "Enter the number corresponding to the time period you'd like to see upcoming games for, or type #{@content.length + 1} to quit: "
-  end
-end
