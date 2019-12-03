470a165b0afd49c9bd5863cf43f5a6c6d09e3aff
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2019-12-03 11:03:45 -0500

    fix some if/else logic for user input

--- lib/main_menu.rb
+++ lib/main_menu.rb
@@ -14,19 +14,22 @@ class MainMenu < Menu
 
   def run
     quit = @content.length
+
     until @input.to_i.between?(1, quit)
       self.print_from_hash
 
       @input = gets.strip
       index = @input.to_i - 1
 
-      if index >= 1 && index < quit
+      if index.between?(0, quit - 1)
         platform_type = @content.to_a[index][0]
+        puts "\nYou selected #{content[platform_type]}\n"
         Scraper.scrape_coming_soon_page(platform_type)
       elsif index == quit
+        puts "\nThanks for using Games Coming Soon. Goodbye!\n"
         exit
       else
-        puts "You made an invalid selection."
+        puts "\nYou made an invalid selection.\n"
       end
     end
   end
