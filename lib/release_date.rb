482d0c71ba069cd3f92d778fb44e0bf44beba3b5
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2019-12-08 14:51:55 -0500

    delete unused methods

--- lib/release_date.rb
+++ lib/release_date.rb
@@ -12,28 +12,16 @@ class ReleaseDate
     @year = d.year
   end
 
-  def self.month_words
-    @@month_words
-  end
-
-  def games
-    Game.all.select { |game| game.release_date == self }
-  end
-
-  def print_date
-    puts "#{self.month}/#{self.day}/#{self.year}"
-  end
-
   def self.all
     @@all
   end
 
-  def save
-    self.class.all << self
+  def self.month_words
+    @@month_words
   end
 
-  def self.find_by_release_date(release_date)
-    self.all.find {|r| r.date == release_date }
+  def save
+    self.class.all << self
   end
 
   def self.create(release_date)
@@ -47,37 +35,21 @@ class ReleaseDate
     r ? r : self.create(release_date)
   end
 
-  def self.find_all_by_date(date)
-    self.all.select { |r| r.date == date }
-  end
-
-  def self.find_all_by_month(month)
-    self.all.select { |r| r.month == month }
-  end
-
   def self.find_games_by_month(month)
     Game.all.select do |game|
       game.release_date.month == self.month_words.index { |word| word == month }
     end
   end
 
-  def self.find_all_by_day(day)
-    self.all.select { |r| r.day == day }
+  def self.find_by_release_date(release_date)
+    self.all.find {|r| r.date == release_date }
   end
 
-  def self.find_all_by_year(year)
-    self.all.select { |r| r.year == year }
+  def games
+    Game.all.select { |game| game.release_date == self }
   end
 
-  def self.find_all_by_month_and_year(month, year)
-    self.all.select { |r| r.month == month && r.year == year }
-  end
-
-  def self.find_all_unique_dates
-    self.all.collect { |r| r.date }.uniq
-  end
-
-  def self.find_all_unique_months
-   self.all.collect { |r| r.month }.uniq
+  def print_date
+    puts "#{self.month}/#{self.day}/#{self.year}"
   end
 end
